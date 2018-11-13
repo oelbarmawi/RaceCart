cf54d12dab360286435c268a06cf0478c0698a5c
Omar  Elbarmawi <oelbarmawi@berkeley.edu>
2018-11-13 17:42:41 -0500

    refactored code

--- watson_nlu.py
+++ watson_nlu.py
@@ -10,36 +10,45 @@ item_locations = {'peanut butter': 'aisle 6', 'milk': 'aisle 9', 'eggs': 'aisle 
 
 
 
-def createResponse():
-natural_language_understanding = NaturalLanguageUnderstandingV1(
-    version='2018-03-16',
-    iam_apikey=API_KEY,
-    url=URL
-)
+def createResponse(raw_text):
+    natural_language_understanding = NaturalLanguageUnderstandingV1(
+        version='2018-03-16',
+        iam_apikey=API_KEY,
+        url=URL
+    )
+
+    response = natural_language_understanding.analyze(
+        # url='www.ibm.com',
+        text=raw_text,
+        features=Features(sentiment=SentimentOptions(targets=TARGETS))).get_result()
+    # print(json.dumps(response, indent=2))
+
+    return getItemLocations(response)
+
+
+def getItemLocations(response):
+    response_items, response_locations = [], []
+    response_targets = response['sentiment']['targets']
+    
+    for r in response_targets:
+    	item = r['text']
+    	if item in item_locations:
+    		response_items.append(item)
+    		response_locations.append(item_locations[item])
+
+    response_string = "For the items: " + ", ".join(response_items) + ".\nGo to " + ", ".join(response_locations)
+    return response_string
 
 
 
-SAMPLE_TEXT = "Where is the peanut butter, and eggs?"
+if __name__ == '__main__':
+    SAMPLE_TEXT1 = "Where is the peanut butter, and eggs?"
+    SAMPLE_TEXT2 = "Where's the milk"
+    SAMPLE_TEXT3 = "I'm looking for eggs today."
 
-response = natural_language_understanding.analyze(
-    # url='www.ibm.com',
-    text=SAMPLE_TEXT,
-    features=Features(sentiment=SentimentOptions(targets=TARGETS))).get_result()
-
-
-response_targets = response['sentiment']['targets']
-response_items = []
-response_locations = []
-for r in response_targets:
-	item = r['text']
-	if item in item_locations:
-		response_items.append(item)
-		response_locations.append(item_locations[item])
-
-response_string = "For the items: " + ", ".join(response_items) + ".\nGo to " + ", ".join(response_locations)
-print(response_string)
+    print(createResponse(SAMPLE_TEXT1), "\n")
+    print(createResponse(SAMPLE_TEXT2), "\n")
+    print(createResponse(SAMPLE_TEXT3), "\n")
 
 
 
-
-# print(json.dumps(response, indent=2))
