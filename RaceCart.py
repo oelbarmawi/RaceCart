4797b4098e4f6c98dc9b16c5d67780839a8477a2
Omar  Elbarmawi <oelbarmawi@berkeley.edu>
2018-11-13 20:16:20 -0500

    Added requirements

--- RaceCart.py
+++ RaceCart.py
@@ -2,6 +2,7 @@ import json
 from flask import Flask, request
 from watson_developer_cloud import NaturalLanguageUnderstandingV1
 from watson_developer_cloud.natural_language_understanding_v1 import Features, EntitiesOptions, SentimentOptions
+from pymessenger.bot import Bot
 
 
 IBM_API_KEY = "HLiuAqMBW9td4_lrcKwo67alISdgzflsv0u3KwxggLkz"
